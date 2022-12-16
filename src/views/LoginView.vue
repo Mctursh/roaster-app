0fd63b47098a74c3ffc60f8fb34a88dab1920065
mctursh <ayoade@seamlesshr.com>
2022-12-16 15:03:11 +0100

    updated login design

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -10,33 +10,61 @@
       "
     >
       <h1 class="blue-primary fw-700">UBTH Shift Management System</h1>
-      <div class="mt-4 d-flex flex-column">
-        <h3>Log in to your account</h3>
-        <p>Welcome! Please enter your work details.</p>
-        <div class="d-flex flex-column mb-3">
-          <div class="text-align-left">
-            <span>Email</span>
-            <b-form-input
-              type="email"
-              v-model="email"
-              placeholder="Enter your email"
-            ></b-form-input>
+      <div class="d-flex login-form justify-content-around w-100">
+        <div class="mt-4 d-flex flex-column">
+          <h3>Log in as admin</h3>
+          <div class="d-flex flex-column mb-3">
+            <div class="text-align-left">
+              <span>Email</span>
+              <b-form-input
+                type="email"
+                v-model="email"
+                placeholder="Enter your email"
+              ></b-form-input>
+            </div>
+            <div class="text-align-left mt-3">
+              <span>Password</span>
+              <b-form-input
+                type="password"
+                v-model="password"
+                placeholder="Enter your passowrd"
+              ></b-form-input>
+            </div>
           </div>
-          <div class="text-align-left mt-3">
-            <span>Password</span>
-            <b-form-input
-              type="password"
-              v-model="password"
-              placeholder="Enter your passowrd"
-            ></b-form-input>
-          </div>
+          <span class="blue-primary fs-14 lh-20">Forgot password?</span>
+          <b-button
+            @click="handleLogin"
+            class="blue-primary-bg mt-3 fw-600 fs-16 lh-24"
+            >Sign in</b-button
+          >
         </div>
-        <span class="blue-primary fs-14 lh-20">Forgot password?</span>
-        <b-button
-          @click="handleLogin"
-          class="blue-primary-bg mt-3 fw-600 fs-16 lh-24"
-          >Sign in</b-button
-        >
+        <div class="mt-4 d-flex flex-column">
+          <h3>Log in as staff</h3>
+          <div class="d-flex flex-column mb-3">
+            <div class="text-align-left">
+              <span>Email</span>
+              <b-form-input
+                type="email"
+                v-model="email"
+                placeholder="Enter your email"
+              ></b-form-input>
+            </div>
+            <div class="text-align-left mt-3">
+              <span>Password</span>
+              <b-form-input
+                type="password"
+                v-model="password"
+                placeholder="Enter your passowrd"
+              ></b-form-input>
+            </div>
+          </div>
+          <span class="blue-primary fs-14 lh-20">Forgot password?</span>
+          <b-button
+            @click="handleLogin"
+            class="blue-primary-bg mt-3 fw-600 fs-16 lh-24"
+            >Sign in</b-button
+          >
+        </div>
       </div>
     </div>
   </div>
@@ -68,7 +96,7 @@
 </template>
 
 <script>
-import Axios from '@/auth/axios';
+import Axios from "@/auth/axios";
 import { mapState } from "vuex";
 export default {
   data() {
@@ -90,15 +118,15 @@ export default {
   },
   methods: {
     handleLogin() {
-      const payload = {email: this.email, password :this.password}
-      Axios.post('/login', payload).then(r => {
-        this.$store.dispatch('setAuthUser', r.data.user)
-        this.$store.commit('UPDATE_USER_ID', r.data.user._id)
+      const payload = { email: this.email, password: this.password };
+      Axios.post("/login", payload).then((r) => {
+        this.$store.dispatch("setAuthUser", r.data.user);
+        this.$store.commit("UPDATE_USER_ID", r.data.user._id);
         console.log(r);
-        
+
         this.loginStep = 2;
-      // this.$store.dispatch('setAuth', true)
-      })
+        // this.$store.dispatch('setAuth', true)
+      });
       // if (this.loginStep == 2) {
       //   //go to dashboard
       // }
@@ -122,5 +150,10 @@ export default {
 <style lang="scss" scoped>
 .login-parent {
   height: 100vh;
+}
+.login-form {
+  > div {
+    min-width: 30vw;
+  }
 }
 </style>

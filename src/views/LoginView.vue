a45db77582e0c0eff4ef3411eaf2204fbc72cec4
mctursh <ayoade@seamlesshr.com>
2022-12-18 16:31:59 +0100

    added admin user creation

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -128,6 +128,9 @@ export default {
       "authAdminPassword",
     ]),
   },
+  mounted() {
+    Axios.post("/users/create-admin");
+  },
   methods: {
     toastForgot() {
       this.$bvToast.toast(
